b04a7f70655433764f8bb4944976110eecc11d08
Shah Arafat <shaharafhat@gmail.com>
2021-06-10 02:05:25 +0600

    tailwind CSS configured and some extra config files added to better usability

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import TodoContainer from "./pages/TodoContainer";
 
 const App = () => {
   return (
-      <div>
-        <h1>Simple Boilerplate React Context Hooks</h1>
-        <TodoContainer />
-      </div>
+    <div className="bg-red-300">
+      <h1>Simple Boilerplate React Context Hooks</h1>
+      <TodoContainer />
+    </div>
   );
 };
 
