e5051f418f0da299aed261256a0854c7c2f3e4a9
Shah Arafat <shaharafhat@gmail.com>
2021-06-10 15:16:59 +0600

    responsive footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Navbar } from "./components";
+import { Footer, Navbar } from "./components";
 
 const App = () => {
   return (
     <div className=" font-roboto">
       <Router>
         <Navbar />
+        <Footer />
       </Router>
       {/* <h1 className=" text-6.5xl">Simple Boilerplate React Context Hooks</h1>
       <TodoContainer /> */}
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
+import Footer from "./common/Footer";
 import Navbar from "./common/Navbar";
 
-export { Navbar };
+export { Navbar, Footer };
